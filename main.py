27918afc5b2ddca05a7e8c5c668f64805f5ae128
molexted <123afoster@gmail.com>
2019-07-19 16:40:02 -0700

    setup iterations

--- main.py
+++ main.py
@@ -30,8 +30,17 @@ print(poslist)
 
 print(transforms.momentasplit(1,0,0))
 
-for i in range(num):
-    for j in range(num*4):
+for i in range(num): #iterate through columns (in sets of three)
+    for j in range(2**num): #iterate through rows
         momentalist[j][0] = 1
+    if i >= 1:
+        subdivdone = 0
+        print(i)
+        subdivs = (2**i)
+        subdivsize = (2**num/subdivs)
+        print(subdivs)
+        for j in range subdivs:
+            subdivdone++
+            for k in range subdivsize:
 
 print(momentalist)
