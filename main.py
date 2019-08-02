456f44ca436df7dcefe14f1433f60e87b0356a2e
molexted <123afoster@gmail.com>
2019-08-02 15:56:45 -0700

    make main somewhat less broken

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import randomint
 import transforms
 
-randomint.randomintrange(5, 0, 10)
+
 
 num = 4
 poslist = []
@@ -14,33 +14,38 @@ for i in range(2**num):
         emptyrow.append(0)
     poslist.append(list(emptyrow))
 
-print(poslist)
 
 momentalist = []
 # 12/3 = 4 <- 4-deep tree
-for i in range(2**num):
+for i in range(2**(num-1)):
     emptyrow = []
     for j in range(num*3):
         emptyrow.append(0)
     momentalist.append(list(emptyrow))
 
-print(poslist)
-
 #momenta creation
 
-print(transforms.momentasplit(1,0,0))
 
-for i in range(num): #iterate through columns (in sets of three)
-    for j in range(2**num): #iterate through rows
+for i in range(num-1): #iterate through columns (in sets of three)
+    for j in range(2**(num-1)): #iterate through rows and set initial momenta to 1,0,0
         momentalist[j][0] = 1
-    if i >= 1:
-        subdivdone = 0
-        print(i)
+    if i >= 1: #for all except initial momenta, do the splitting
+        subdivdone = 0 #need to reset
         subdivs = (2**i)
-        subdivsize = (2**num/subdivs)
-        print(subdivs)
-        for j in range subdivs:
-            subdivdone++
-            for k in range subdivsize:
+        subdivsize = int(2**num/subdivs)
+        for j in range(int(subdivs/2)):#iterate through subdivisions/2 (as pairs of subdivisions share momentum)
+            subdivdone = subdivdone + 1
+            xprev = momentalist[3*j][i*3-3] 
+            yprev = momentalist[3*j][i*3-2]
+            zprev = momentalist[3*j][i*3-1]
+            momentax = transforms.momentasplit(xprev,yprev,zprev)[0]
+            momentay = transforms.momentasplit(xprev,yprev,zprev)[1]
+            momentaz = transforms.momentasplit(xprev,yprev,zprev)[2]
+            print(j)
+            print(i)
+            for k in range(subdivsize):#identical outputs for everything here
+                momentalist[(3*j)+k][i*3+2] = momentax
+                momentalist[(3*j)+k][i*3+1] = momentay
+                momentalist[(3*j)+k][i*3] = momentaz
 
 print(momentalist)
